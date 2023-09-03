fed08b43c4df3bee70a97feb08b6ca9611f2af69
Pervejhasan <mdpervej12441@gmail.com>
2023-09-03 17:44:12 +0600

    apply redux-thunk middleware

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React-Redux</h2>
+    </div>
+  );
+}
+
+export default App;
+
+//step1: install package
+//step2: constants define
+//step3: async action creator
+//step4: reducer
+//step5: create store
+//step6: provide store
+//step7: use store
+//step8: adding css
